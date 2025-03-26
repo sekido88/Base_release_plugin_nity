cf3592fcfcfe18811e996aaf00689b726fdfb255
f1minh88 <f1minh88@gmail.com>
2025-03-26 12:06:50 +0700

    fix: abcde

--- Runtime/CommonUtils.cs
+++ Runtime/CommonUtils.cs
@@ -63,4 +63,13 @@ public static class CommonUtils
         (a, b) = (b, a);
     }
     
+    /// <summary>
+    /// Clamps a value between min and max, inclusive
+    /// </summary>
+    public static T Clamp<T>(T value, T min, T max) where T : IComparable<T>
+    {
+        if (value.CompareTo(min) < 0) return min;
+        if (value.CompareTo(max) > 0) return max;
+        return value;
+    }
 }
